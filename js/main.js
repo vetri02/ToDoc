678a0fa7ae50216a2936dbaa4369c33f9a409b55
Vetrichelvan Jeyapalpandy <vetri02@gmail.com>
2012-02-01 01:34:10 +0530

    icons and final

--- js/main.js
+++ js/main.js
@@ -1,80 +1,87 @@
 var toDo = {
-    init: function () {
+    init: function() {
 
         this.taskval = null;
         this.textar = '<p class="charLeft">You have <span id="charsLeft"></span> chars left.</p><textarea name="taskAddition" id="task' + this.taskNo + '" class="textA" placeholder="Add Task Here..."></textarea>';
 
-        $("#done").click(function () {
+        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        var height = (window.innerHeight > 0) ? window.innerHeight : screen.height;
+
+        $("#done").click(function() {
             toDo.done();
         });
-        $("#add").click(function () {
+        $("#add").click(function() {
             toDo.addTask();
         });
-        $("#back").click(function () {
+        $("#back").click(function() {
             toDo.goBack()
-        })
-        $("#delete").click(function () {
+            })
+            $("#delete").click(function() {
             toDo.deleteTask();
         });
-        $("#save").click(function () {
+        $("#save").click(function() {
             toDo.setVal();
         });
 
-        $("#content").delegate("ul.taskList li input[type=checkbox]", "click", function () {
-            $(this).parent().toggleClass('strike');
+        $("#content").delegate("ul.taskList li input[type=checkbox]", "click", function() {
+            if ($(this).parent().hasClass('strike')) {
+                $(this).parent().removeClass('strike');
+                toDo.check($(this), false);
+            } else {
+                $(this).parent().addClass('strike');
+                toDo.check($(this), true);
+            }
+
         });
 
-        $("#content").delegate("ul.taskList li span.listInd", "click", function () {
-        	if($(this).siblings("input[type=checkbox]").prop("checked")){
-            	$(this).siblings("input[type=checkbox]").prop("checked", false);
-            	$(this).parent().removeClass('strike');
-            }else{
-            	$(this).siblings("input[type=checkbox]").prop("checked", true);
-            	$(this).parent().addClass('strike');
+        $("#content").delegate("ul.taskList li span.listInd", "click", function() {
+            if ($(this).siblings("input[type=checkbox]").prop("checked")) {
+                $(this).siblings("input[type=checkbox]").prop("checked", false);
+                $(this).parent().removeClass('strike');
+                toDo.check($(this).siblings("input[type=checkbox]"), false);
+            } else {
+                $(this).siblings("input[type=checkbox]").prop("checked", true);
+                $(this).parent().addClass('strike');
+                toDo.check($(this).siblings("input[type=checkbox]"), true);
             }
-            
+
         });
 
         this.showList();
 
     },
-    showList: function () {
+    showList: function() {
         $('#content').html('<ul class="taskList"></ul>');
         var p = amplify.store();
-        //var arr = $.makeArray(p);
-        console.log(p);
         for (var key in p) {
             if (p.hasOwnProperty(key)) {
                 this.taskNo = key;
                 var pval = p[key];
-                console.log(typeof(this.taskval));
-                for (key in pval){
-                	if (pval.hasOwnProperty(key)) {
-                		console.log(pval.val);
-                		console.log(pval.done);
-                		this.taskval = pval.val;
-                	}
+                for (key in pval) {
+                    if (pval.hasOwnProperty(key)) {
+                        this.taskval = pval.val;
+                        this.taskDone = pval.done;
+                    }
                 }
-                this.taskLi = '<li><input type="checkbox" name="todo' + this.taskNo + '" value="' + this.taskNo + '" id="todo' + this.taskNo + '"><label for="todo' + this.taskNo + '">Check' + this.taskNo + '</label><span class="listInd">' + this.taskval + '</span></li>';
+                if (pval.done === "true") {
+                    this.taskLi = '<li class="strike"><input checked="true" type="checkbox" name="todo' + this.taskNo + '" value="' + this.taskNo + '" id="todo' + this.taskNo + '"><label for="todo' + this.taskNo + '">Check' + this.taskNo + '</label><span class="listInd">' + this.taskval + '</span></li>';
+
+                } else {
+                    this.taskLi = '<li><input type="checkbox" name="todo' + this.taskNo + '" value="' + this.taskNo + '" id="todo' + this.taskNo + '"><label for="todo' + this.taskNo + '">Check' + this.taskNo + '</label><span class="listInd">' + this.taskval + '</span></li>';
+                }
+
                 $('ul.taskList').append(this.taskLi);
-                //$('ul.taskList li input[type=checkbox]').click(function(){
-                //$(this).parent().toggleClass('strike');
-                //});
-                //console.log(key + ":" + p[key]);
             }
         }
     },
-    goBack: function () {
-        console.info("back");
+    goBack: function() {
         $('.main').show();
         $('.taskEditAdd').hide();
 
         this.showList();
 
-
     },
-    addTask: function () {
-        console.info("add");
+    addTask: function() {
         $('.taskEditAdd').show();
         $('.taskEditAdd .edit').hide();
         $('.main').hide();
@@ -82,61 +89,63 @@ var toDo = {
         $('#content').html(this.textar);
         $('.textA').limit('70', '#charsLeft');
 
-
     },
-    deleteTask: function () {
-        console.info("delete");
+    deleteTask: function() {
         this.checkedLength = $("#content input:checked").length;
         $("#content input:checked").each(
-
-        function () {
+function() {
             var chVal = $(this).val();
             amplify.store(chVal, null)
-        })
-        this.goBack();
+            })
+            this.goBack();
     },
-    edit: function () {
+    edit: function() {
         console.info("edit");
     },
-    setVal: function () {
-        console.info("setVal");
+    setVal: function() {
         var p = amplify.store();
         var arr = [];
-        console.log(p);
         for (var key in p) {
             arr.push(parseInt(key));
         }
         this.maxKey = Math.max.apply(Math, arr);
         if (this.maxKey > 0) {
-            //console.info(this.taskNo+'if');
             this.taskNo = this.maxKey + 1;
-            console.info(this.taskNo + 'ifafter');
             toDo.getVal();
         } else {
-            console.info(this.taskNo + 'else');
             this.taskNo = 1;
             toDo.getVal();
         }
         this.goBack();
 
     },
-    getVal: function () {
+    getVal: function() {
         this.taskval = $("textarea").attr('value');
         if (this.taskval != '' && this.taskval != null) {
-            //amplify.store(this.taskNo, this.taskval);
-
-            amplify.store( this.taskNo, { 
-  				val: this.taskval, done: "false" 
-			});
+            amplify.store(this.taskNo, {
+                val: this.taskval,
+                done: "false"
+            });
 
         }
     },
-    check: function () {
+    check: function(item, strikeVal) {
+        if (strikeVal) {
+            amplify.store(item.prop('value'), {
+                val: item.siblings("span.listInd").html(),
+                done: "true"
+            });
+        } else {
+            amplify.store(item.prop('value'), {
+                val: item.siblings("span.listInd").html(),
+                done: "false"
+            });
+        }
 
     }
 
 };
 
-$(document).ready(function () {
+$(document).ready(function() {
     toDo.init();
 });
